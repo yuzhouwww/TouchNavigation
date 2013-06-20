e57d1ec93d843593af392c8ca3b3734a5580ba00
yuzhouwww <469258988@qq.com>
2013-06-20 16:10:06 +0800

    Initial Commit

---
+++ TouchNavigation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TouchNavigation
+//
+//  Created by yuzhou on 13-6-20.
+//  Copyright (c) 2013年 wzyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TouchNavigation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TouchNavigation
+//
+//  Created by yuzhou on 13-6-20.
+//  Copyright (c) 2013年 wzyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
